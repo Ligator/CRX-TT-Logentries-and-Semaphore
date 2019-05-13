f0b95c26d450ea82297db4e3e407a0435cae5a05
Abel Pacheco <abelpacheco@outlook.com>
2019-05-12 22:57:30 -0500

    CRX-TT-Logentries-and-Semaphore

---
+++ src/background.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  // Logentries
+  $("div.EventsCard--eventsTitle").after("<a href='javascript:void(0);' class='logentries-chrome-ext' style='font-size: 15px; white-space: nowrap;'>Expand All Results</a>");
+  $(".logentries-chrome-ext").on("click", logentries_expand_results);
+
+  function logentries_expand_results(){
+    $(".r7-icon-search-rawlog-mag").click();
+  }
+
+  // Semaphore
+  $("div#build-state-app").after("<a href='javascript:void(0);' class='semaphore-chrome-ext' style='font-size: 15px; white-space: nowrap;'>Expand All Failures</a>");
+  $(".semaphore-chrome-ext").on("click", semaphore_expand_failures);
+
+  function semaphore_expand_failures(){
+    $(".c-filter-projects_icon_failed").click();
+    $(".c-results_list_command").click()
+  }
+});
